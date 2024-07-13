6dccd6f057c4995ed4f1ac101b736630098011d2
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-13 21:59:50 +0300

    refactoring: change the component to functional

--- class-components/src/components/Fallback.tsx
+++ class-components/src/components/Fallback.tsx
@@ -1,18 +1,14 @@
-import { Component } from 'react';
-
-class Fallback extends Component {
-  reloadPage() {
+const Fallback = () => {
+  const reloadPage = () => {
     history.go(0);
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <h1>Something went wrong!</h1>
-        <button onClick={this.reloadPage}>Return to main page</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+      <button onClick={reloadPage}>Return to main page</button>
+    </div>
+  );
+};
 
 export default Fallback;
