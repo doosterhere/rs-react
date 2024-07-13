54ec9fc7409ff49e92467a157d30634ad9095be1
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-13 22:00:25 +0300

    refactoring: change the component to functional

--- class-components/src/components/List.tsx
+++ class-components/src/components/List.tsx
@@ -1,26 +1,20 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import { PlanetType } from '../types';
 import { ListItem } from '../components';
 
 interface IListProps {
-  itemsList: PlanetType[];
+  itemsList: PlanetType[] | [];
   isLoading: boolean;
 }
 
-class List extends Component<IListProps> {
-  render() {
-    return (
-      <div className="list">
-        {!!this.props.itemsList.length &&
-          this.props.itemsList.map(item => (
-            <ListItem {...item} key={item.name} />
-          ))}
-        {!this.props.itemsList.length && !this.props.isLoading && (
-          <div>No results found</div>
-        )}
-      </div>
-    );
-  }
-}
+const List: FC<IListProps> = ({ itemsList, isLoading }) => {
+  return (
+    <div className="list">
+      {!!itemsList.length &&
+        itemsList.map(item => <ListItem {...item} key={item.name} />)}
+      {!itemsList.length && !isLoading && <div>No results found</div>}
+    </div>
+  );
+};
 
 export default List;
