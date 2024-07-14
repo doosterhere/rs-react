b06f79830197530e5b6a91032cc8182ad3904970
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-14 14:57:08 +0300

    feat: add additional route

--- class-components/src/components/AppRouter.tsx
+++ class-components/src/components/AppRouter.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import { MainPage, Page404 } from '../pages';
+import ListItemDetailed from './ListItemDetailed';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
+          <Route path="" element={<MainPage />}>
+            <Route path=":name" element={<ListItemDetailed />} />
+          </Route>
         </Route>
         <Route path="*" element={<Page404 />} />
       </Routes>
