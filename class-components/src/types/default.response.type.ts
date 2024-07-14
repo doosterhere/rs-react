51216ddf2c59e9cd148d6ea06cde45f13daa5395
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-15 02:33:25 +0300

    refactor: modify type

--- class-components/src/types/default.response.type.ts
+++ class-components/src/types/default.response.type.ts
@@ -1,19 +1,6 @@
-import PlanetType from './planet.type';
-
-type DefaultResponseType =
-  | {
-      count: number;
-      next: string | null;
-      previous: string | null;
-      results: PlanetType[];
-      detail: never;
-    }
-  | {
-      count: never;
-      next: never;
-      previous: never;
-      results: never;
-      detail: string;
-    };
+type DefaultResponseType<T> = {
+  count: number;
+  results: T[];
+};
 
 export default DefaultResponseType;
