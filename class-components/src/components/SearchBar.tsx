38ec5b442294e4ddb1667d3fc9527255503425a2
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-15 02:34:52 +0300

    refactor: improve component

--- class-components/src/components/SearchBar.tsx
+++ class-components/src/components/SearchBar.tsx
@@ -1,46 +1,78 @@
-import { useEffect } from 'react';
+import { FC, useState, useEffect, useRef, FormEvent } from 'react';
 import { useLocalStorage } from '../hooks';
 import { useSearchParams } from 'react-router-dom';
+import { DefaultResponseType, PlanetType } from '../types';
+import Loader from './Loader';
 
-const SearchBar = () => {
-  const {
-    value: searchQuery,
-    setValue: setSearchQuery,
-    restored,
-  } = useLocalStorage('searchQuery');
-  const [, setSearchParams] = useSearchParams();
+const LS_KEY = 'searchQuery';
+
+interface ISearchBarProps {
+  setData: React.Dispatch<React.SetStateAction<DefaultResponseType<PlanetType>>>;
+}
+
+const SearchBar: FC<ISearchBarProps> = ({ setData }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const { value: searchQuery, setValue: setSearchQuery, restored } = useLocalStorage(LS_KEY);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    if (restored || !searchQuery) handleSearch();
+    if (restored || !searchQuery) {
+      if (inputRef.current) inputRef.current.value = searchQuery || '';
+      if (searchQuery) searchParams.set('search', searchQuery);
+      fetchData(searchQuery, searchParams.get('page') || '1');
+    }
   }, [restored]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value.trim());
+  useEffect(() => {
+    if (!searchParams.get('page')) {
+      setSearchParams(prev => `${prev}&page=1`);
+    }
+  }, [searchParams, setSearchParams]);
+
+  useEffect(() => {
+    if (searchQuery !== null) {
+      fetchData(searchQuery, searchParams.get('page') || '1');
+    }
+  }, [searchQuery, searchParams]);
+
+  const fetchData = async (query: string, page: string) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`https://swapi.dev/api/planets/?search=${query}&page=${page}`);
+      const results = await response.json();
+      setData(results);
+    } catch (error) {
+      setData({ count: 0, results: [] });
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  const handleSearch = () => {
-    setSearchParams({
-      search: searchQuery,
-      page: '1',
-    });
-  };
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') handleSearch();
+    if (inputRef.current) {
+      setSearchQuery(inputRef.current.value);
+      setSearchParams({ search: inputRef.current.value, page: '1' });
+    }
   };
 
   return (
     <div className="search-bar">
-      <input
-        type="text"
-        placeholder="Search for a Star Wars planet..."
-        value={searchQuery}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-      />
-      <button type="button" onClick={handleSearch}>
-        Search
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          autoComplete="off"
+          placeholder="Search for a Star Wars planet..."
+          ref={inputRef}
+          disabled={isLoading}
+        />
+        <button type="submit" disabled={isLoading}>
+          Search
+        </button>
+      </form>
+      {isLoading && <Loader />}
     </div>
   );
 };
