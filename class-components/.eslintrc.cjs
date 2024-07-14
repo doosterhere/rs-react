20de6146e93233ea1b2c1871bb1d99ce3c05d964
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-15 02:27:16 +0300

    fix: format

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -4,11 +4,7 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'react-compiler'],
