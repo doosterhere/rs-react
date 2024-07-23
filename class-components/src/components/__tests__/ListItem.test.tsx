e969efd5ae6a6de6c68f8d1a4ddfac941e5338d0
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-23 14:50:55 +0300

    test: complement ListItem

--- class-components/src/components/__tests__/ListItem.test.tsx
+++ class-components/src/components/__tests__/ListItem.test.tsx
@@ -4,8 +4,13 @@ import { renderWithRouter } from '../../utils';
 
 import ListItem from '../ListItem';
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn().mockReturnValue({ id: '1' }),
+}));
+
 describe('ListItem', () => {
-  it('should renders correctly', () => {
+  it('should render with correct href when id matches', () => {
     const props = {
       name: 'test',
       id: '1',
@@ -15,6 +20,17 @@ describe('ListItem', () => {
 
     expect(screen.getByText(/test/i)).toBeInTheDocument();
 
-    expect(screen.getByRole('link')).toMatchSnapshot();
+    expect(screen.getByRole('link')).toHaveAttribute('href', '/');
+  });
+
+  it('should render with correct href when id does not match', () => {
+    const props = {
+      name: 'test',
+      id: '2',
+    };
+
+    renderWithRouter(<ListItem {...props} />);
+
+    expect(screen.getByRole('link')).toHaveAttribute('href', '/detail/2');
   });
 });
