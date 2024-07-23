6f95a54264eee0e8a3290472b581264d25c6ff38
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-23 12:23:28 +0300

    test: correction

--- class-components/src/components/__tests__/ListItemDetailed.test.tsx
+++ class-components/src/components/__tests__/ListItemDetailed.test.tsx
@@ -7,14 +7,13 @@ import * as apiMock from '../../api';
 
 const details = {
   name: 'test-planet',
-  rotation_period: '1000',
-  orbital_period: '900',
   diameter: '800',
   climate: 'test-climate',
   gravity: '700',
   terrain: 'test-terrain',
   surface_water: '600',
   population: '500',
+  url: 'https://swapi.dev/api/planets/1/',
 };
 
 const apiSpy = jest.spyOn(apiMock, 'getDetailedData');
@@ -29,5 +28,11 @@ describe('ListItemDetailed', () => {
     await waitForElementToBeRemoved(() => screen.queryByRole('progressbar'));
 
     expect(screen.getByText(/test-planet/i)).toBeInTheDocument();
+    expect(screen.getByText(/800/i)).toBeInTheDocument();
+    expect(screen.getByText(/test-climate/i)).toBeInTheDocument();
+    expect(screen.getByText(/700/i)).toBeInTheDocument();
+    expect(screen.getByText(/test-terrain/i)).toBeInTheDocument();
+    expect(screen.getByText(/600/i)).toBeInTheDocument();
+    expect(screen.getByText(/500/i)).toBeInTheDocument();
   });
 });
