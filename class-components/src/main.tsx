b4713eab6378842288eef1de554a4eaa2972a917
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-25 12:53:08 +0300

    feat: use redux provider

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,9 +1,19 @@
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './main.css';
+
+import { store, persistor } from './store';
 import { ErrorBoundary, AppRouter } from './components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary>
-    <AppRouter />
-  </ErrorBoundary>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ErrorBoundary>
+        <AppRouter />
+      </ErrorBoundary>
+    </PersistGate>
+  </Provider>,
 );
