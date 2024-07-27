74c2d71baf1f7c2c6463870b9de9ef5509cfc93f
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-27 21:26:18 +0300

    feat: implement theme value change

--- class-components/src/components/Header/Header.tsx
+++ class-components/src/components/Header/Header.tsx
@@ -1,16 +1,54 @@
+import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import clsx from 'clsx';
 
 import classes from './Header.module.css';
 
-const Header = () => (
-  <header className={classes.header} role="heading">
-    <div>
-      <Link to={'/'}>
-        <img src="/images/star-wars-logo.png" alt="Star Wars logo" />
-      </Link>
-      Star Wars planet finder app
-    </div>
-  </header>
-);
+import { ThemeContext } from '../ThemeContext';
+import { useLocalStorage } from '../../hooks';
+
+const LS_KEY = 'theme';
+
+const Header = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const { value: themeStyle, setValue: setThemeStyle, restored } = useLocalStorage(LS_KEY);
+
+  useEffect(() => {
+    if (restored) {
+      if (theme.value !== themeStyle) {
+        toggleTheme();
+      }
+    }
+  }, [restored]);
+
+  const toggle = () => {
+    toggleTheme();
+    setThemeStyle(() => (theme.value === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <header className={clsx(classes.header, classes[theme.value])} role="heading">
+      <div>
+        <Link to={'/'}>
+          <img src="/images/star-wars-logo.png" alt="Star Wars logo" />
+        </Link>
+        <div>Star Wars planet finder app</div>
+        <div className={classes.theme} onClick={toggle}>
+          {theme.value === 'light' && (
+            <div className={classes.button}>
+              <i className="fa-solid fa-sun" />
+            </div>
+          )}
+          {theme.value === 'dark' && (
+            <div className={classes.button} onClick={toggle}>
+              <i className="fa-solid fa-moon"></i>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export { Header };
