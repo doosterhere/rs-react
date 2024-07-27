76a447dd77a14185bdfcf4aa42679b3f2e045ac0
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-27 21:28:35 +0300

    feat: use theme changing

--- class-components/src/components/ListItemDetailed/ListItemDetailed.tsx
+++ class-components/src/components/ListItemDetailed/ListItemDetailed.tsx
@@ -1,17 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams, Link, useSearchParams } from 'react-router-dom';
+
+import clsx from 'clsx';
 
 import classes from './ListItemDetailed.module.css';
 
 import { Loader } from '..';
 import { PlanetType } from '../../types';
 import { planetApi } from '../../api';
+import { ThemeContext } from '../ThemeContext';
 
 const ListItemDetailed = () => {
   const { id } = useParams();
   const [searchParams] = useSearchParams();
   const [detailedData, setDetailedData] = useState<PlanetType>({} as PlanetType);
   const { fulfilledTimeStamp: loaded, data } = planetApi.useGetPlanetQuery(id!);
+  const { theme } = useContext(ThemeContext);
 
   useEffect(() => {
     if (data) {
@@ -24,7 +28,7 @@ const ListItemDetailed = () => {
       {!loaded ? (
         <Loader />
       ) : (
-        <div className={classes.content}>
+        <div className={clsx(classes.content, classes[theme.value])}>
           <div>Name: {detailedData?.name}</div>
           <div>Diameter: {detailedData?.diameter}</div>
           <div>Gravity: {detailedData?.gravity}</div>
