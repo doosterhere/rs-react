a45a92b893091736c97abbbfc845510c48227799
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-27 21:24:40 +0300

    feat: provide ThemeProvider

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -6,13 +6,16 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './main.css';
 
 import { store, persistor } from './store';
+import { ThemeProvider } from './components';
 import { ErrorBoundary, AppRouter } from './components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <ErrorBoundary>
-        <AppRouter />
+        <ThemeProvider>
+          <AppRouter />
+        </ThemeProvider>
       </ErrorBoundary>
     </PersistGate>
   </Provider>,
