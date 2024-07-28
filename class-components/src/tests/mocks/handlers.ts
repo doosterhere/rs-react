212370fbfcc5c65c20cba9c2c8a8e88279ab7db1
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-07-28 19:35:29 +0300

    test: change msw config ans mocks

--- class-components/src/tests/mocks/handlers.ts
+++ class-components/src/tests/mocks/handlers.ts
@@ -1,12 +1,12 @@
 import { HttpResponse, http } from 'msw';
-import json from './response.json';
+import { mockPlanets, mockPlanet } from './mockData.ts';
 
 export const handlers = [
   http.get('*/planets*', () => {
-    return HttpResponse.json(json);
+    return HttpResponse.json(mockPlanets);
   }),
 
-  http.get('*/planets/:id*', () => {
-    return HttpResponse.json(json);
+  http.get('*/planets/1*', () => {
+    return HttpResponse.json(mockPlanet);
   }),
 ];
--- class-components/src/tests/mocks/mockData.ts
+++ class-components/src/tests/mocks/mockData.ts
@@ -1,40 +1,51 @@
-import { DefaultResponseType, PlanetType } from '../types';
+import { DefaultResponseType, PlanetType } from '../../types';
 
-const mockDetailedData: PlanetType = {
-  name: 'test-planet',
-  diameter: '100',
-  climate: 'test-climate',
-  gravity: '200',
-  terrain: 'test-terrain',
-  surface_water: '300',
-  population: '400',
+const mockedPlanet: PlanetType = {
+  name: 'Mocked Planet',
+  rotation_period: '23',
+  orbital_period: '304',
+  diameter: '10465',
+  climate: 'arid',
+  gravity: '1 standard',
+  terrain: 'desert',
+  surface_water: '1',
+  population: '200000',
+  residents: [
+    'https://swapi.dev/api/people/1/',
+    'https://swapi.dev/api/people/2/',
+    'https://swapi.dev/api/people/4/',
+    'https://swapi.dev/api/people/6/',
+    'https://swapi.dev/api/people/7/',
+    'https://swapi.dev/api/people/8/',
+    'https://swapi.dev/api/people/9/',
+    'https://swapi.dev/api/people/11/',
+    'https://swapi.dev/api/people/43/',
+    'https://swapi.dev/api/people/62/',
+  ],
+  films: [
+    'https://swapi.dev/api/films/1/',
+    'https://swapi.dev/api/films/3/',
+    'https://swapi.dev/api/films/4/',
+    'https://swapi.dev/api/films/5/',
+    'https://swapi.dev/api/films/6/',
+  ],
+  created: '2014-12-09T13:50:49.641000Z',
+  edited: '2014-12-20T20:58:18.411000Z',
   url: 'https://swapi.dev/api/planets/1/',
 };
 
-const mockData: DefaultResponseType<PlanetType> = {
-  count: 2,
-  results: [
-    {
-      name: 'test-planet-1',
-      diameter: '100',
-      climate: 'test-climate',
-      gravity: '200',
-      terrain: 'test-terrain',
-      surface_water: '300',
-      population: '400',
-      url: 'https://swapi.dev/api/planets/1/',
-    },
-    {
-      name: 'test-planet-2',
-      diameter: '1100',
-      climate: 'test-climate',
-      gravity: '1200',
-      terrain: 'test-terrain',
-      surface_water: '1300',
-      population: '1400',
-      url: 'https://swapi.dev/api/planets/2/',
-    },
-  ],
+const planetsNotFound: DefaultResponseType<PlanetType> = {
+  count: 0,
+  next: null,
+  previous: null,
+  results: [],
 };
 
-export { mockDetailedData, mockData };
+const mockedPlanets: DefaultResponseType<PlanetType> = {
+  count: 1,
+  next: 'https://swapi.dev/api/planets/?search=&page=2',
+  previous: null,
+  results: [mockedPlanet],
+};
+
+export { mockedPlanet, mockedPlanets, planetsNotFound };
