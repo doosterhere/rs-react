d546ec83c3087764c1388631988b923dfc81cf4f
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-06 11:10:30 +0300

    refactor: remove loader

--- class-components/src/components/ListItemDetailed/ListItemDetailed.tsx
+++ class-components/src/components/ListItemDetailed/ListItemDetailed.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { FC, useContext } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -6,34 +6,28 @@ import clsx from 'clsx';
 
 import classes from './ListItemDetailed.module.css';
 
-import { Loader } from '..';
-import { useGetPlanetQuery } from '../../api';
 import { ThemeContext } from '../ThemeContext';
+import { PlanetType } from '../../types';
 
-const ListItemDetailed = () => {
-  const { query } = useRouter();
-  const { data, fulfilledTimeStamp: loading } = useGetPlanetQuery(query.detail!.toString());
+const ListItemDetailed: FC<{ data: PlanetType }> = ({ data }) => {
   const { theme } = useContext(ThemeContext);
+  const { query } = useRouter();
   const search = query.search?.toString() || '';
   const page = query.page?.toString() || '1';
   const queryString = `search=${search}&page=${page}`;
 
   return (
     <>
-      {!loading ? (
-        <Loader />
-      ) : (
-        <div className={clsx(classes.content, classes[theme.value])}>
-          <div>Name: {data?.name}</div>
-          <div>Diameter: {data?.diameter}</div>
-          <div>Gravity: {data?.gravity}</div>
-          <div>Population: {data?.population}</div>
-          <div>Climate: {data?.climate}</div>
-          <div>Terrain: {data?.terrain}</div>
-          <div>Surfase water: {data?.surface_water}</div>
-          <Link href={`/?${queryString}`}>Close</Link>
-        </div>
-      )}
+      <div className={clsx(classes.content, classes[theme.value])}>
+        <div>Name: {data?.name}</div>
+        <div>Diameter: {data?.diameter}</div>
+        <div>Gravity: {data?.gravity}</div>
+        <div>Population: {data?.population}</div>
+        <div>Climate: {data?.climate}</div>
+        <div>Terrain: {data?.terrain}</div>
+        <div>Surfase water: {data?.surface_water}</div>
+        <Link href={`/?${queryString}`}>Close</Link>
+      </div>
     </>
   );
 };
