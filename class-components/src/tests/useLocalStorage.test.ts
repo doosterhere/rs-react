b11af96dbd06b7f2a3fdbccd99b9ead301a5afa9
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-06 12:02:22 +0300

    test: fix test

--- class-components/src/tests/useLocalStorage.test.ts
+++ class-components/src/tests/useLocalStorage.test.ts
@@ -6,32 +6,29 @@ describe('useLocalStorage', () => {
   it('should return an object with correct properties', () => {
     const { result } = renderHook(() => useLocalStorage('key', 'test-value'));
 
-    expect(result.current).toHaveProperty('value');
-    expect(typeof result.current.value).toBe('string');
+    expect(result.current).toHaveProperty('storedValue');
+    expect(typeof result.current.storedValue).toBe('string');
 
-    expect(result.current).toHaveProperty('restored');
-    expect(typeof result.current.restored).toBe('boolean');
-
-    expect(result.current).toHaveProperty('setValue');
-    expect(typeof result.current.setValue).toBe('function');
+    expect(result.current).toHaveProperty('setStoredValue');
+    expect(typeof result.current.setStoredValue).toBe('function');
   });
 
   it('should get data from localStorage and set data before unmount', () => {
-    localStorage.setItem('key', 'old_value');
+    localStorage.setItem('key', JSON.stringify('old_value'));
 
-    const { result, unmount } = renderHook(() => useLocalStorage('key'));
+    const { result, unmount } = renderHook(() => useLocalStorage('key', 'old_value'));
 
-    expect(result.current.value).toBe('old_value');
+    expect(result.current.storedValue).toBe('old_value');
 
     act(() => {
-      result.current.setValue('new_value');
+      result.current.setStoredValue('new_value');
     });
 
-    expect(result.current.value).toBe('new_value');
+    expect(result.current.storedValue).toBe('new_value');
 
     unmount();
 
-    expect(localStorage.getItem('key')).toBe('new_value');
+    expect(JSON.parse(localStorage.getItem('key') || '')).toBe('new_value');
 
     localStorage.removeItem('key');
 
