a663b41fd45bfaf761e3b8077bd1be67b8ca9165
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-06 11:14:20 +0300

    feat: convert component for nextJS requirements

--- class-components/src/components/SearchBar/SearchBar.tsx
+++ class-components/src/components/SearchBar/SearchBar.tsx
@@ -1,89 +1,47 @@
-import { FC, useEffect, useRef, FormEvent, useContext } from 'react';
+import { useState, FormEvent, useContext, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 import clsx from 'clsx';
 
 import classes from './SearchBar.module.css';
 
-import { useAppDispatch, useLocalStorage } from '../../hooks';
-import { DefaultResponseType, PlanetType } from '../../types';
-import { Loader } from '..';
-import { useGetPlanetsQuery } from '../../api';
-import { setCurrentPageItems } from '../../store';
 import { ThemeContext } from '../ThemeContext';
+import { useLocalStorage } from '../../hooks';
 
 const LS_KEY = 'searchQuery';
 
-interface ISearchBarProps {
-  setData: React.Dispatch<React.SetStateAction<DefaultResponseType<PlanetType>>>;
-}
-
-const SearchBar: FC<ISearchBarProps> = ({ setData }) => {
+const SearchBar = () => {
   const router = useRouter();
   const { query } = router;
-  const inputRef = useRef<HTMLInputElement>(null);
-  const dispatcher = useAppDispatch();
   const { theme } = useContext(ThemeContext);
-  const { value: searchQuery, setValue: setSearchQuery, restored } = useLocalStorage(LS_KEY);
-  const { fulfilledTimeStamp: loaded, data } = useGetPlanetsQuery({
-    search: searchQuery,
-    page: query.page?.toString() || '1',
-  });
+  const [searchQuery, setSearchQuery] = useLocalStorage(LS_KEY, '');
+  const [value, setValue] = useState(searchQuery);
 
   useEffect(() => {
-    if (restored || !searchQuery) {
-      if (inputRef.current) {
-        inputRef.current.value = searchQuery || '';
-      }
-
-      const newQuery = { ...query };
-
-      if (query.search?.toString() !== searchQuery) {
-        newQuery.search = searchQuery;
-      }
-
-      if (!query.page) {
-        newQuery.page = '1';
-      }
-
-      router.push({ pathname: router.pathname, query: newQuery });
-    }
-  }, [restored, searchQuery]);
-
-  useEffect(() => {
-    if (data) {
-      setData(data);
-      dispatcher(setCurrentPageItems(data.results));
-    }
-  }, [dispatcher, setData, data]);
+    setValue(searchQuery);
+  }, [searchQuery]);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    if (inputRef.current) {
-      setSearchQuery(inputRef.current.value);
-      // setSearchParams({ search: inputRef.current.value, page: '1' });
-      const newQuery = { ...query, search: inputRef.current.value, page: '1' };
-      router.push({ pathname: router.pathname, query: newQuery });
-    }
+    setSearchQuery(value);
+    const newQuery = { ...query, search: value, page: '1' };
+    router.push({ pathname: router.pathname, query: newQuery });
   };
 
   return (
     <div className={clsx(classes.search, classes[theme.value])}>
       <form onSubmit={handleSubmit} role="form">
         <input
+          value={value || ''}
+          onChange={e => setValue(e.target.value)}
           className={classes.searchInput}
           type="search"
           autoComplete="off"
           placeholder="Search for a Star Wars planet..."
-          ref={inputRef}
-          disabled={!loaded}
         />
-        <button type="submit" disabled={!loaded}>
-          Search
-        </button>
+        <button type="submit">Search</button>
       </form>
-      {!loaded && <Loader />}
     </div>
   );
 };
