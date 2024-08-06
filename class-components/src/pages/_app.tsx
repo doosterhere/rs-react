aeee55fc071f4476d5d6845e89dcbb1a39d5f3a5
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-06 12:49:31 +0300

    feat: add loader

--- class-components/src/pages/_app.tsx
+++ class-components/src/pages/_app.tsx
@@ -1,23 +1,50 @@
+import Router from 'next/router';
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import '../styles/global.css';
 
 import { wrapper, persistor } from '../store/store';
-import { ThemeProvider } from '../components';
+import { Loader, ThemeProvider } from '../components';
 import { ErrorBoundary } from '../components';
 
 export default function App({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
   const { pageProps } = props;
 
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+
+    const end = () => {
+      setLoading(false);
+    };
+
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ErrorBoundary>
           <ThemeProvider>
-            <Component {...pageProps} />
+            <>
+              {loading && <Loader />}
+              <Component {...pageProps} />
+            </>
           </ThemeProvider>
         </ErrorBoundary>
       </PersistGate>
