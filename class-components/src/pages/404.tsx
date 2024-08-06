5a7b9f034a3fbd4ab9b879664003f69163858893
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-06 11:11:45 +0300

    refactor: remove unusual import

--- class-components/src/pages/404.tsx
+++ class-components/src/pages/404.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link';
-import clsx from 'clsx';
 
 import classes from './404.module.css';
 
 export default function Page404() {
   return (
-    <div className={clsx(classes.page)}>
+    <div className={classes.page}>
       <h1>This page doesn&apos;t exist</h1>
       <Link href={'/'}>Return to main page</Link>
     </div>
