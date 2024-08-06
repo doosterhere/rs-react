839e006670c11679aa2adb4083336e0bf4b7f1d3
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-06 22:04:28 +0300

    test: remove unusual folder

--- class-components/src/tests/setupTests.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-
-import { mockServer } from './mocks/server';
-
-beforeAll(() => mockServer.listen());
-afterEach(() => mockServer.resetHandlers());
-afterAll(() => mockServer.close());
