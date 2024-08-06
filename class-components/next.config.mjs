8e1421af16b701ef5f1f5171c6b2d4fb91fd7f03
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-06 16:59:21 +0300

    chore: remove experimental next config option for babel

--- class-components/next.config.mjs
+++ class-components/next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export', // Outputs a Single-Page Application (SPA).
-  // distDir: './dist', // Changes the build output directory to `./dist/`.
-  experimental: {
-    forceSwcTransforms: true,
-  },
 };
 
 export default nextConfig;
