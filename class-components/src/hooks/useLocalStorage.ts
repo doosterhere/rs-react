c072b6ea1e01af56b8434d1d86b8dee566ce49ad
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-06 11:06:30 +0300

    refactor: improve the hook

--- class-components/src/hooks/useLocalStorage.ts
+++ class-components/src/hooks/useLocalStorage.ts
@@ -1,27 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
-const useLocalStorage = (key: string, defaultValue = '') => {
-  const [value, setValue] = useState(defaultValue);
-  const [restored, setRestored] = useState(false);
+export const useLocalStorage = <T>(key: string, defaultValue: T) => {
+  const [storedValue, setStoredValue] = useState<T | undefined>();
+
+  const setValue = useCallback(
+    (value: T) => {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    },
+    [key],
+  );
 
   useEffect(() => {
-    const storedQuery = localStorage.getItem(key);
+    const value = window.localStorage.getItem(key);
 
-    if (storedQuery || storedQuery === '') {
-      setValue(storedQuery);
-      setRestored(true);
+    if (value) {
+      try {
+        const parsed = JSON.parse(value) as T;
+        setStoredValue(parsed);
+      } catch (error) {
+        console.log(error);
+        setStoredValue(defaultValue);
+      }
+    } else {
+      setStoredValue(defaultValue);
     }
-  }, [key]);
+  }, [defaultValue, key]);
 
   useEffect(() => {
-    localStorage.setItem(key, value);
+    if (typeof storedValue === 'string') {
+      setValue(storedValue);
+    }
+  }, [storedValue, setValue]);
 
-    return () => {
-      localStorage.setItem(key, value);
-    };
-  }, [key, value]);
-
-  return { value, setValue, restored };
+  return [storedValue as T, setStoredValue] as const;
 };
-
-export { useLocalStorage };
