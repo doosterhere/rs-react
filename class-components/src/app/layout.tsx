0b2659c028e68bb4e0763340fecc44fbf28adf57
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-07 17:23:31 +0300

    feat: create root layout

---
+++ class-components/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+
+import { AppProvider } from '../components/AppProvider';
+import { Header } from '../components/Header';
+
+import '../styles/global.css';
+
+export const metadata: Metadata = {
+  title: 'SW search app',
+  description: 'Next.js search app for StarWars planets',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      url: '/favicon.png',
+    },
+  ],
+};
+
+const montserrat = Montserrat({ subsets: ['latin'], style: 'normal', display: 'swap', weight: ['400', '700'] });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={montserrat.className}>
+      <head>
+        <script src="https://kit.fontawesome.com/c7802b1604.js" crossOrigin="anonymous" defer />
+      </head>
+      <body>
+        <AppProvider>
+          <Header />
+          {children}
+        </AppProvider>
+      </body>
+    </html>
+  );
+}
