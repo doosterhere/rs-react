036d7b94238173c08a931141623392e9306bdad8
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-09 11:59:51 +0300

    test: fix test

--- class-components/src/tests/Page404.test.tsx
+++ class-components/src/tests/Page404.test.tsx
@@ -1,16 +1,14 @@
-// import { screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
 import { renderWithProvider } from '../utils';
-import Page404 from '../pages';
-
-// jest.mock('react-redux');
+import Page404 from '../app/not-found';
 
 describe('Page404', () => {
   it('should render correctly', () => {
     renderWithProvider(<Page404 />);
 
-    // expect(screen.getByText(/This page doesn't exist/i)).toBeInTheDocument();
+    expect(screen.getByText(/This page doesn't exist/i)).toBeInTheDocument();
 
-    // expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
   });
 });
