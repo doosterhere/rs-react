69de9f782bbacf5e2279fca127da4fbbac7f2993
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-09 14:39:41 +0300

    test: fix ListItem

--- class-components/src/tests/ListItem.test.tsx
+++ class-components/src/tests/ListItem.test.tsx
@@ -1,14 +1,27 @@
 import { screen, render } from '@testing-library/react';
 import * as reduxHooks from 'react-redux';
 import userEvent from '@testing-library/user-event';
+import { useParams, useSearchParams } from 'next/navigation';
 
-import { ListItem } from '../components';
+import { ListItem } from '../components/ListItem';
 import { FullPlanetInfo } from '../types';
 
 jest.mock('react-redux');
 
+jest.mock('next/navigation', () => ({
+  __esModule: true,
+  useParams: jest.fn(),
+  useSearchParams: jest.fn(),
+}));
+
 describe('ListItem', () => {
-  it('should render with correct href when id does not match', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render with correct href when on the main page', () => {
+    (useParams as jest.Mock).mockReturnValue({ id: '' });
+    (useSearchParams as jest.Mock).mockReturnValue({ get: () => '' });
     jest.spyOn(reduxHooks, 'useSelector').mockReturnValueOnce(true);
 
     const props = {
@@ -20,6 +33,21 @@ describe('ListItem', () => {
 
     expect(container).toMatchSnapshot();
     expect(screen.getByRole('link')).toHaveAttribute('href', '/detail/1?search=&page=1');
+  });
+
+  it('should render with correct href when on the detail page', () => {
+    (useParams as jest.Mock).mockReturnValue({ id: '1' });
+    (useSearchParams as jest.Mock).mockReturnValue({ get: () => '' });
+    jest.spyOn(reduxHooks, 'useSelector').mockReturnValueOnce(true);
+
+    const props = {
+      name: 'test',
+      id: '1',
+    } as FullPlanetInfo;
+
+    render(<ListItem {...props} />);
+
+    expect(screen.getByRole('link')).toHaveAttribute('href', '/?search=&page=1');
   });
 
   it('should call useDispatch when checkbox is clicked', async () => {
