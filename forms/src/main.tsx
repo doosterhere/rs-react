1005d3d17fd897363ddffa7fa63cee032ab2946d
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-13 22:38:58 +0300

    feat: use AppRouter in the entry point (main.tsx)

--- forms/src/main.tsx
+++ forms/src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import './styles/styles.scss';
+
+import { AppRouter } from './components/AppRouter';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>The forms</div>
+    <AppRouter />
   </StrictMode>,
 );
