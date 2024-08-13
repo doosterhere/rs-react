31e1e88256c982a3902f7783e8983a353e417017
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-13 22:37:05 +0300

    feat: add Layout component

---
+++ forms/src/components/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import { Header } from '../Header';
+import { Footer } from '../Footer';
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Outlet />
+      </div>
+      <Footer />
+    </>
+  );
+}
+export { Layout };
