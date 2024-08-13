3ef0262e21769ae13f479cd7ab6cea9982bb3968
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-13 22:34:29 +0300

    feat: add Navigation component

---
+++ forms/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,24 @@
+import { useLocation, NavLink } from 'react-router-dom';
+
+import classes from './Navigation.module.scss';
+
+import { NavLinkType } from '../../types/navlink.types';
+
+type Props = {
+  navLinks: NavLinkType[];
+};
+
+function Navigation({ navLinks }: Props) {
+  const location = useLocation();
+
+  return (
+    <>
+      {navLinks.map(({ label, to }) => (
+        <NavLink to={to} className={location?.pathname === to ? classes.active : ''} key={label}>
+          {label}
+        </NavLink>
+      ))}
+    </>
+  );
+}
+export { Navigation };
