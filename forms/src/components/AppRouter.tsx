3db8dafeaaad0cc82fff886cc1a26d6d6d596369
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-15 21:04:45 +0300

    feat: use redux provider

--- forms/src/components/AppRouter.tsx
+++ forms/src/components/AppRouter.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { ROUTES } from '@/constants';
 
+import { configStore } from '@/store/store';
 import { Layout } from './Layout';
 import { ControlledForm } from '@/pages/ControlledForm';
 import { UncontrolledForm } from '@/pages/UncontrolledForm';
@@ -15,12 +17,17 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       { path: ROUTES.UNCONTROLLED_FORM, element: <UncontrolledForm /> },
       { path: ROUTES.CONTROLLED_FORM, element: <ControlledForm /> },
+      { path: '*', element: <HomePage /> },
     ],
   },
 ]);
 
 const AppRouter = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={configStore()}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 };
 
 export { AppRouter };
