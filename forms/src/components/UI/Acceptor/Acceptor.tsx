e794ca4a45bdf8669eadef9c0e9b3321ee49700f
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-16 22:50:35 +0300

    feat: modify element to use with controlled/uncontrolled approach

--- forms/src/components/UI/Acceptor/Acceptor.tsx
+++ forms/src/components/UI/Acceptor/Acceptor.tsx
@@ -1,18 +1,21 @@
+import { forwardRef } from 'react';
+
 import classes from './Acceptor.module.scss';
 
 type Props = {
   label: string;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-function Acceptor({ label, ...attr }: Props) {
+const Acceptor = forwardRef(({ label, ...attr }: Props, ref?: React.ForwardedRef<HTMLInputElement>) => {
   return (
     <div className={classes.acceptor}>
       <label className={classes.label}>
-        <input type="checkbox" className={classes.input} required {...attr} />
+        <input type="checkbox" className={classes.input} required ref={ref || undefined} {...attr} />
         <span />
         {label}
       </label>
     </div>
   );
-}
+});
+
 export { Acceptor };
