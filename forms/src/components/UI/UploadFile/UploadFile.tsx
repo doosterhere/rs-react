304e98a34236fa245aec162408f65699028ae29b
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-17 17:08:22 +0300

    feat: modity UploadFile component

--- forms/src/components/UI/UploadFile/UploadFile.tsx
+++ forms/src/components/UI/UploadFile/UploadFile.tsx
@@ -4,25 +4,24 @@ import classes from './UploadFile.module.scss';
 
 type Props = {
   label: string;
-  required?: boolean;
+  message?: string;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-const UploadFile = forwardRef(
-  ({ label, required = false, ...attr }: Props, ref?: React.ForwardedRef<HTMLInputElement>) => {
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-      const size = e.target.files?.[0]?.size || 0;
-      console.log(size);
-    };
+const UploadFile = forwardRef(({ label, message, ...attr }: Props, ref?: React.ForwardedRef<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const size = e.target.files?.[0]?.size || 0;
+    console.log(size);
+  };
 
-    return (
-      <div className={classes.uploader}>
-        <label className={classes.label}>
-          {(required ? '* ' : '') + label}:
-          <input type="file" onChange={handleChange} className={classes.input} ref={ref} {...attr} />
-        </label>
-      </div>
-    );
-  },
-);
+  return (
+    <div className={classes.uploader}>
+      <label className={classes.label}>
+        {label}:
+        <input type="file" onChange={handleChange} className={classes.input} ref={ref} {...attr} />
+        {message && <span className={classes.error}>{message}</span>}
+      </label>
+    </div>
+  );
+});
 
 export { UploadFile };
