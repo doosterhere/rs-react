9bd512b284fe14397ee3f9ca55c73d87d4de654d
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-18 00:12:06 +0300

    feat: add UncontrolledForm page

--- forms/src/pages/UncontrolledForm.tsx
+++ forms/src/pages/UncontrolledForm.tsx
@@ -1,4 +1,8 @@
+import { useCountries } from '../hooks/useCountries';
+
 function UncontrolledForm() {
+  const countries = useCountries();
+
   return <div>Uncontrolled form</div>;
 }
 export { UncontrolledForm };
