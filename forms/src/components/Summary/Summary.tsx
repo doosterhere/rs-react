4d8bdd83edf45a530b7346640e322ddc8ef35c88
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-17 21:47:45 +0300

    feat: add Summary component

---
+++ forms/src/components/Summary/Summary.tsx
@@ -0,0 +1,33 @@
+import classes from './Summary.module.scss';
+
+import { selectFormData } from '@/store/reducers/formReducer';
+import { useAppSelector } from '@/store/store';
+
+function Summary() {
+  const data = useAppSelector(selectFormData);
+
+  return (
+    <div className={classes.summary}>
+      <div>
+        <b>Full name:</b> {data.name}
+      </div>
+      <div>
+        <b>Age:</b> {data.age}
+      </div>
+      <div>
+        <b>e-mail:</b> {data.email}
+      </div>
+      <div>
+        <b>Gender:</b> {data.gender}
+      </div>
+      <div>
+        <b>Country:</b> {data.country}
+      </div>
+      <div>
+        <b>Avatar:</b>
+        <img src={data.picture} />
+      </div>
+    </div>
+  );
+}
+export { Summary };
