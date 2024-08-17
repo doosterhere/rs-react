ca21821289cec25a778eb133df6feb0e141e0125
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-17 17:07:42 +0300

    feat: modity RadioGroup component

--- forms/src/components/UI/RadioGroup/RadioGroup.tsx
+++ forms/src/components/UI/RadioGroup/RadioGroup.tsx
@@ -4,20 +4,19 @@ import classes from './RadioGroup.module.scss';
 type Props = {
   label: string;
   options: string[];
-  name: string;
+  message?: string;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 const RadioGroup = forwardRef(
-  ({ label, options, name, ...attr }: Props, ref?: React.ForwardedRef<HTMLInputElement>) => {
+  ({ label, options, message, ...attr }: Props, ref?: React.ForwardedRef<HTMLInputElement>) => {
     return (
       <div className={classes.group}>
-        {'* ' + label}:
+        {label}:
         <div className={classes.groups}>
           {options.map((option, index) => {
             return (
               <div className={classes.option} key={option}>
                 <input
                   type="radio"
-                  name={name}
                   id={option}
                   value={option}
                   defaultChecked={index === 0}
@@ -32,6 +31,7 @@ const RadioGroup = forwardRef(
             );
           })}
         </div>
+        {message && <span className={classes.error}>{message}</span>}
       </div>
     );
   },
