65b4d75ba136f1b1f8a65a7310ad6ef94da69a3d
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-18 00:11:17 +0300

    feat: change HomePage page

--- forms/src/pages/HomePage.tsx
+++ forms/src/pages/HomePage.tsx
@@ -1,7 +1,16 @@
+import { Summary } from '../components/Summary';
+import classes from './HomePage.module.scss';
+
+import { useAppSelector } from '@/store/store';
+
 function HomePage() {
+  const data = useAppSelector(state => state.form.formData);
+
   return (
-    <div>
-      <h1>Comparison of controlled and uncontrolled forms</h1>
+    <div className={classes.data}>
+      <h1>Submitted data:</h1>
+      {!data.name && <div>There is no data yet</div>}
+      {!!data.name && <Summary />}
     </div>
   );
 }
