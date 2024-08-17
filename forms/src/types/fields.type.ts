2ffd04f07691c7e612bc44c04bcb6895fe5ce429
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-17 17:08:44 +0300

    feat: add Fields type

---
+++ forms/src/types/fields.type.ts
@@ -0,0 +1,11 @@
+export enum Fields {
+  name = 'name',
+  age = 'age',
+  email = 'email',
+  password = 'password',
+  passwordRepeat = 'passwordRepeat',
+  gender = 'gender',
+  country = 'country',
+  picture = 'picture',
+  agreed = 'agreed',
+}
