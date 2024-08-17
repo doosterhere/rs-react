1410511ff6f33d0ee301df792b1866c7890e92e5
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-18 00:10:04 +0300

    feat: use PasswordStrengthMeter component

--- forms/src/components/UI/Input/Input.tsx
+++ forms/src/components/UI/Input/Input.tsx
@@ -2,16 +2,20 @@ import { forwardRef } from 'react';
 
 import classes from './Input.module.scss';
 
+import { PasswordStrengthMeter } from '../PasswordStrengthMeter';
+
 type Props = {
   label: string;
   message?: string;
+  password?: string;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = forwardRef(({ label, message, ...attr }: Props, ref?: React.ForwardedRef<HTMLInputElement>) => {
+const Input = forwardRef(({ label, message, password, ...attr }: Props, ref?: React.ForwardedRef<HTMLInputElement>) => {
   return (
     <label className={classes.label}>
       {label}:
       <input className={classes.input} ref={ref} {...attr} />
+      {attr.type === 'password' && password && <PasswordStrengthMeter password={password || ''} />}
       {message && <span className={classes.error}>{message}</span>}
     </label>
   );
