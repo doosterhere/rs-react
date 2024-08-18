7bf41f51093e33273a7bfdbcd279fb74f19bc1db
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-19 00:54:22 +0300

    refactor: remove the outer react fragment

--- forms/src/pages/ControlledForm.tsx
+++ forms/src/pages/ControlledForm.tsx
@@ -44,66 +44,64 @@ function ControlledForm() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmitHandler)}>
-        <Input
-          type="text"
-          label="Full Name"
-          placeholder="ex. John Doe"
-          {...register(Fields.name)}
-          message={errors.name?.message}
-        />
-        <Input type="number" min={1} max={130} label="Age" {...register(Fields.age)} message={errors.age?.message} />
-        <Input
-          type="email"
-          label="Email"
-          placeholder="ex. mail@example.com"
-          {...register(Fields.email)}
-          message={errors.email?.message}
-        />
-        <Input
-          type="password"
-          label="Password"
-          {...register(Fields.password)}
-          message={errors.password?.message}
-          password={watch(Fields.password)}
-        />
-        <Input
-          type="password"
-          label="Repeat Password"
-          {...register(Fields.passwordRepeat)}
-          message={errors.passwordRepeat?.message}
-        />
-        <RadioGroup
-          label="Gender"
-          options={['Male', 'Female', 'Other']}
-          {...register(Fields.gender)}
-          message={errors.gender?.message}
-        />
-        <Picker
-          label="Country"
-          dataList={countries}
-          value={currentCountry || ''}
-          onChanging={(value: string) => setValue('country', value)}
-          {...register(Fields.country)}
-          message={errors.country?.message}
-        />
-        <UploadFile
-          label="Upload your avatar"
-          accept=".png,.jpeg"
-          {...register(Fields.picture)}
-          message={errors.picture?.message}
-        />
-        <Acceptor
-          label="I accept Terms and Conditions agreement"
-          {...register(Fields.agreed)}
-          message={errors.agreed?.message}
-        />
-        <Button type="submit" disabled={Object.keys(errors).length > 0}>
-          Submit
-        </Button>
-      </form>
-    </>
+    <form onSubmit={handleSubmit(onSubmitHandler)}>
+      <Input
+        type="text"
+        label="Full Name"
+        placeholder="ex. John Doe"
+        {...register(Fields.name)}
+        message={errors.name?.message}
+      />
+      <Input type="number" min={1} max={130} label="Age" {...register(Fields.age)} message={errors.age?.message} />
+      <Input
+        type="email"
+        label="Email"
+        placeholder="ex. mail@example.com"
+        {...register(Fields.email)}
+        message={errors.email?.message}
+      />
+      <Input
+        type="password"
+        label="Password"
+        {...register(Fields.password)}
+        message={errors.password?.message}
+        password={watch(Fields.password)}
+      />
+      <Input
+        type="password"
+        label="Repeat Password"
+        {...register(Fields.passwordRepeat)}
+        message={errors.passwordRepeat?.message}
+      />
+      <RadioGroup
+        label="Gender"
+        options={['Male', 'Female', 'Other']}
+        {...register(Fields.gender)}
+        message={errors.gender?.message}
+      />
+      <Picker
+        label="Country"
+        dataList={countries}
+        value={currentCountry || ''}
+        onChanging={(value: string) => setValue('country', value)}
+        {...register(Fields.country)}
+        message={errors.country?.message}
+      />
+      <UploadFile
+        label="Upload your avatar"
+        accept=".png,.jpeg"
+        {...register(Fields.picture)}
+        message={errors.picture?.message}
+      />
+      <Acceptor
+        label="I accept Terms and Conditions agreement"
+        {...register(Fields.agreed)}
+        message={errors.agreed?.message}
+      />
+      <Button type="submit" disabled={Object.keys(errors).length > 0}>
+        Submit
+      </Button>
+    </form>
   );
 }
 export { ControlledForm };
