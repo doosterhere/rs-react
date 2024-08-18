79c239e80b32aa9c695316a58140531566c6a962
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-19 00:53:41 +0300

    refactor: remove unused handler

--- forms/src/components/UI/UploadFile/UploadFile.tsx
+++ forms/src/components/UI/UploadFile/UploadFile.tsx
@@ -8,16 +8,11 @@ type Props = {
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
 const UploadFile = forwardRef(({ label, message, ...attr }: Props, ref?: React.ForwardedRef<HTMLInputElement>) => {
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const size = e.target.files?.[0]?.size || 0;
-    console.log(size);
-  };
-
   return (
     <div className={classes.uploader}>
       <label className={classes.label}>
         {label}:
-        <input type="file" onChange={handleChange} className={classes.input} ref={ref} {...attr} />
+        <input type="file" className={classes.input} ref={ref} {...attr} />
         {message && <span className={classes.error}>{message}</span>}
       </label>
     </div>
