7792344a516a1c597decf71bab422c902cd9f2e7
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-19 01:23:09 +0300

    feat: change Summary component

--- forms/src/components/Summary/Summary.tsx
+++ forms/src/components/Summary/Summary.tsx
@@ -8,25 +8,30 @@ function Summary() {
 
   return (
     <div className={classes.summary}>
-      <div>
-        <b>Full name:</b> {data.name}
-      </div>
-      <div>
-        <b>Age:</b> {data.age}
-      </div>
-      <div>
-        <b>e-mail:</b> <a href="mailto:{data.email}">{data.email}</a>
-      </div>
-      <div>
-        <b>Gender:</b> {data.gender}
-      </div>
-      <div>
-        <b>Country:</b> {data.country}
-      </div>
-      <div>
-        <b>Avatar:</b>
-        <img src={data.picture} alt="Avatar" />
-      </div>
+      {data.map((data, index) => (
+        <div key={index}>
+          <h3>Form {index + 1}</h3>
+          <div>
+            <b>Full name:</b> {data.name}
+          </div>
+          <div>
+            <b>Age:</b> {data.age}
+          </div>
+          <div>
+            <b>e-mail:</b> <a href="mailto:{data.email}">{data.email}</a>
+          </div>
+          <div>
+            <b>Gender:</b> {data.gender}
+          </div>
+          <div>
+            <b>Country:</b> {data.country}
+          </div>
+          <div>
+            <b>Avatar:</b>
+            <img src={data.picture} alt="Avatar" />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
