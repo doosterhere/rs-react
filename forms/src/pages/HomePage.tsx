22cc78f7a72589afc65c6679d19cfb292b0558ad
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-19 01:22:49 +0300

    feat: change HomePage page

--- forms/src/pages/HomePage.tsx
+++ forms/src/pages/HomePage.tsx
@@ -9,8 +9,8 @@ function HomePage() {
   return (
     <div className={classes.data}>
       <h1>Submitted data:</h1>
-      {!data.name && <div>There is no data yet</div>}
-      {!!data.name && <Summary />}
+      {data.length === 0 && <div>There is no data yet</div>}
+      {data.length > 0 && <Summary />}
     </div>
   );
 }
