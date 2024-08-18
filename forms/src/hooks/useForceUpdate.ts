736a006551c5b4d42bfda563b70778ee6b8a4f03
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-19 00:55:50 +0300

    feat: hook for update of an uncontrolled form

---
+++ forms/src/hooks/useForceUpdate.ts
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+const useForceUpdate = () => {
+  const [, setToggle] = useState(false);
+
+  return () => setToggle(toggle => !toggle);
+};
+
+export { useForceUpdate };
